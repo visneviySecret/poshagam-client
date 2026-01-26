33450136f6e0ebc5fbe70b997a3dce1c5fa25f39
E. Belousov <visneviy.secret@yandex.ru>
2026-01-26 22:26:59 +0300

    Update bread crumbs

--- components/RouterMenu/index.vue
+++ components/RouterMenu/index.vue
@@ -9,6 +9,20 @@
       <img src="~/assets/icons/navArrow.svg" />
       <nuxt-link to="/settings">Настройки</nuxt-link>
     </div>
+    <div v-if="routeName === '/products'">
+      <img src="~/assets/icons/navArrow.svg" />
+      <nuxt-link to="/products">Мои товары</nuxt-link>
+    </div>
+    <template v-if="routeName === '/products/create'">
+      <div>
+        <img src="~/assets/icons/navArrow.svg" />
+        <nuxt-link to="/products">Мои товары</nuxt-link>
+      </div>
+      <div>
+        <img src="~/assets/icons/navArrow.svg" />
+        <nuxt-link to="/products/create">Новый товар</nuxt-link>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -28,13 +42,14 @@ div {
   gap: 10px;
 }
 a {
-  text-decoration: none;
-}
-.router-link-exact-active {
   color: #797b86;
+  text-decoration: none;
   &:hover {
     opacity: 0.7;
   }
+}
+.router-link-exact-active {
+  color: #537694;
 }
 
 .home {
